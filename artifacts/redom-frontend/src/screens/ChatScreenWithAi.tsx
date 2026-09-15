eb44edded2f6593eca668c81a7f19938293dbcab
ReDom <jessicasanchez89965@gmail.com>
2026-09-15 07:43:18 -0500

    feat(messaging): show encryption notice inside chats

--- artifacts/redom-frontend/src/screens/ChatScreenWithAi.tsx
+++ artifacts/redom-frontend/src/screens/ChatScreenWithAi.tsx
@@ -4,12 +4,16 @@ import type { RootStackParamList } from "../routing/types";
 import { ChatScreen } from "./ChatScreen";
 import { ChatLockGate } from "./ChatLockGate";
 import { DirectChatCallBridge } from "./DirectChatCallBridge";
+import { ChatEncryptionNotice } from "../components/ChatEncryptionNotice";
 
 export function ChatScreenWithAi(props: NativeStackScreenProps<RootStackParamList, "Chat">) {
   return (
     <ChatLockGate conversationId={props.route.params.conversationId}>
       <View style={{ flex: 1 }}>
         <ChatScreen {...props} />
+        <View pointerEvents="box-none" style={{ position: "absolute", top: 62, left: 0, right: 0 }}>
+          <ChatEncryptionNotice />
+        </View>
         <DirectChatCallBridge conversationId={props.route.params.conversationId} />
       </View>
     </ChatLockGate>
