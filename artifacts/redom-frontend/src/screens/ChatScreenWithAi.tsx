fc862aae2cdc9c94689004a75d336be6cf14569c
ReDom <jessicasanchez89965@gmail.com>
2026-09-14 19:53:03 -0500

    Connect chat header video call action

--- artifacts/redom-frontend/src/screens/ChatScreenWithAi.tsx
+++ artifacts/redom-frontend/src/screens/ChatScreenWithAi.tsx
@@ -1,12 +1,17 @@
+import { View } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { RootStackParamList } from "../routing/types";
 import { ChatScreen } from "./ChatScreen";
 import { ChatLockGate } from "./ChatLockGate";
+import { DirectChatCallBridge } from "./DirectChatCallBridge";
 
 export function ChatScreenWithAi(props: NativeStackScreenProps<RootStackParamList, "Chat">) {
   return (
     <ChatLockGate conversationId={props.route.params.conversationId}>
-      <ChatScreen {...props} />
+      <View style={{ flex: 1 }}>
+        <ChatScreen {...props} />
+        <DirectChatCallBridge conversationId={props.route.params.conversationId} />
+      </View>
     </ChatLockGate>
   );
 }
