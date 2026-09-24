e98fc3de0488be8c472ac3611587ac1ebe700f23
ReDom <jessicasanchez89965@gmail.com>
2026-09-24 11:45:08 -0500

    Fix View Once video event imports

--- artifacts/redom-frontend/src/messages/viewOnceVideo.tsx
+++ artifacts/redom-frontend/src/messages/viewOnceVideo.tsx
@@ -1,3 +1,5 @@
+import { useEventListener } from "expo";
+import { useState } from "react";
 import { useVideoPlayer, VideoView } from "expo-video";
 import { StyleSheet, View } from "react-native";
 
